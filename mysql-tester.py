ee4c6bd84bf24126a307c4435fe139cd88546350
Peter Predtechensky <Petr.Predtechensky@gmail.com>
2019-10-23 13:08:38 +0300

    Restore secret key

--- mysql-tester.py
+++ mysql-tester.py
@@ -19,6 +19,10 @@ class ReusableForm(Form):
 
 # Instantiate our Node
 app = Flask(__name__)
+
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
+
 LOG = create_logger(app)
 LOG.setLevel(logging.INFO)
 
